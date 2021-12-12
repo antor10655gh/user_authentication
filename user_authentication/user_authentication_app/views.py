fbffe20ff3d0903190258b483e246b38c1865775
antor10655gh <creativefreelancerantor@gmail.com>
2021-12-13 02:58:23 +0600

    add new user_authentication code

--- user_authentication/user_authentication_app/views.py
+++ user_authentication/user_authentication_app/views.py
@@ -1,13 +1,45 @@
 from django.shortcuts import render
 from user_authentication_app.forms import UserForm, UserInfoForm
 from user_authentication_app.models import UserInfo
+from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from django.contrib.auth.decorators import login_required
 from django.urls import reverse
 # Create your views here.
+def log_in(request):
+    return render(request, 'user_authentication_app/login.html', context={})
+
+def user_login(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return HttpResponseRedirect(reverse('user_authentication_app:index'))
+            else:
+                return HttpResponse('Account is not active!!')
+        else:
+            HttpResponse('Login Details are Wrong!')
+    else:
+        return render(request, 'user_authentication_app/login.html', context={})
+@login_required
+def user_logout(request):
+    logout(request)
+    return HttpResponseRedirect(reverse('user_authentication_app:index'))
+
 def index(request):
     diction = {'title':'home page'}
+    if request.user.is_authenticated:
+        current_user = request.user
+        user_id = current_user.id
+        user_basic_info = User.objects.get(pk=user_id)
+        user_more_info = UserInfo.objects.get(user__pk=user_id)
+        diction = {'user_basic_info':user_basic_info,'user_more_info':user_more_info}
     return render(request, 'user_authentication_app/index.html', context=diction)
 
 def register(request):
@@ -25,8 +57,8 @@ def register(request):
             userinfo = userinfo_form.save(commit=False)
             userinfo.user = user
 
-            if 'profile_pic' in request.FILES:
-                userinfo.profile_pic = request.FILES['profile_pic']
+            if 'profile_pics' in request.FILES:
+                userinfo.profile_pic = request.FILES['profile_pics']
 
             userinfo.save()
             registered = True
@@ -36,26 +68,3 @@ def register(request):
 
     diction = {'user_form':user_form,'userinfo_form':userinfo_form, 'registered':registered}
     return render(request, 'user_authentication_app/register.html', context=diction)
-
-def log_in(request):
-    diction = {'login':'Login Form'}
-    return render(request, 'user_authentication_app/login.html', context=diction)
-
-def user_login(request):
-    if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-
-        user = authenticate(username=username, password=password)
-
-        if user:
-            if user.is_active:
-                log_in(request, user)
-                print("Login Successfull!")
-                return HttpResponseRedirect(reverse('user_authentication_app:index'))
-            else:
-                return HttpResponse('Account is not active!!')
-        else:
-            HttpResponse('Login Details are Wrong!')
-    else:
-        return render(request, 'user_authentication_app/login.html', context={})
